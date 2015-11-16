e0a0be4d24c1924c7dd3ad9b33448e1cd49ca804
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2015-11-16 18:21:02 +0000

    effective date validation added

--- sdltc-assets/js/src/services/validationServices/leaseDatesValidationService.js
+++ sdltc-assets/js/src/services/validationServices/leaseDatesValidationService.js
@@ -6,21 +6,30 @@
     app.service('leaseDatesValidationService', function() {
 
         var validate = function(data) {
-            var state = {};
-            var buildState = require("../../utilities/buildState");
+            var state = {},
+                startDateValid = false,
+                endDateValid = false;
+
+            var buildState = require('../../utilities/buildState');
 
             // validate the startDate
             if (data.startDate === '' || data.startDate === undefined) {
-                state.startDate = "You must complete the start date field";
+                state.startDate = 'You must complete the start date field';
             } else if (data.startDate === 'bad date') {
-                state.startDate = "You have entered an incorrect start date, check your entry and correct it";
+                state.startDate = 'You have entered an incorrect start date, check your entry and correct it';
+            } else {
+                startDateValid = true;
             }
 
             // validate the endDate
             if (data.endDate === '' || data.endDate === undefined) {
-                state.endDate = "You must complete the end date field";
+                state.endDate = 'You must complete the end date field';
             } else if (data.endDate === 'bad date') {
-                state.endDate = "You have entered an incorrect end date, check your entry and correct it";
+                state.endDate = 'You have entered an incorrect end date, check your entry and correct it';
+            } else if (startDateValid && data.endDate < data.startDate) {
+                state.endDate = 'The lease end date cannot be before the lease start date';
+            } else if (data.effectiveDate && data.endDate < data.effectiveDate) {
+                state.endDate = 'The lease end date cannot be before the effective date';
             }
 
             return buildState(state);
