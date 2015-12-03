200745347df44c51322765129a01b14a1e2d6806
alex.fisher <alex.fisher@digital.hmrc.gov.uk>
2015-12-03 15:25:59 +0000

    reverting lease date changes

--- sdltc-assets/js/src/services/validationServices/leaseDatesValidationService.js
+++ sdltc-assets/js/src/services/validationServices/leaseDatesValidationService.js
@@ -7,28 +7,28 @@
 
         var validate = function(data) {
             var state = {},
-                startDateValid = false;
+                startDateValid = false,
+                endDateValid = false;
 
             var buildState = require('../../utilities/buildState');
-            var validator = require("../../utilities/validator")();
 
             // validate the startDate
-            if (validator.isNotPopulated(data.startDate)) {
+            if (data.startDate === '' || data.startDate === undefined) {
                 state.startDate = 'You must complete the start date field';
-            } else if (validator.isInvalidParsedDate(data.startDate)) {
+            } else if (data.startDate === 'bad date') {
                 state.startDate = 'You have entered an incorrect start date, check your entry and correct it';
             } else {
                 startDateValid = true;
             }
 
             // validate the endDate
-            if (validator.isNotPopulated(data.endDate)) {
+            if (data.endDate === '' || data.endDate === undefined) {
                 state.endDate = 'You must complete the end date field';
-            } else if (validator.isInvalidParsedDate(data.endDate)) {
+            } else if (data.endDate === 'bad date') {
                 state.endDate = 'You have entered an incorrect end date, check your entry and correct it';
-            } else if (startDateValid && validator.isLessThanDate(data.endDate, data.startDate)) {
+            } else if (startDateValid && data.endDate < data.startDate) {
                 state.endDate = 'The lease end date cannot be before the lease start date';
-            } else if (validator.isPopulated(data.effectiveDate) && validator.isLessThanDate(data.endDate, data.effectiveDate)) {
+            } else if (data.effectiveDate && data.endDate < data.effectiveDate) {
                 state.endDate = 'The lease end date cannot be before the effective date';
             }
 
