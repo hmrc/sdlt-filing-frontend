a705343756408506743597bc0c06341fa7c36566
alex.fisher <alex.fisher@digital.hmrc.gov.uk>
2015-12-04 09:50:16 +0000

    re-refactoring date and lease date services

--- sdltc-assets/js/src/services/validationServices/leaseDatesValidationService.js
+++ sdltc-assets/js/src/services/validationServices/leaseDatesValidationService.js
@@ -11,24 +11,25 @@
                 endDateValid = false;
 
             var buildState = require('../../utilities/buildState');
+            var validator = require("../../utilities/validator")();
 
             // validate the startDate
-            if (data.startDate === '' || data.startDate === undefined) {
+            if (validator.isNotPopulated(data.startDate)) {
                 state.startDate = 'You must complete the start date field';
-            } else if (data.startDate === 'bad date') {
+            } else if (validator.isInvalidParsedDate(data.startDate)) {
                 state.startDate = 'You have entered an incorrect start date, check your entry and correct it';
             } else {
                 startDateValid = true;
             }
 
             // validate the endDate
-            if (data.endDate === '' || data.endDate === undefined) {
+            if (validator.isNotPopulated(data.endDate)) {
                 state.endDate = 'You must complete the end date field';
-            } else if (data.endDate === 'bad date') {
+            } else if (validator.isInvalidParsedDate(data.endDate)) {
                 state.endDate = 'You have entered an incorrect end date, check your entry and correct it';
-            } else if (startDateValid && data.endDate < data.startDate) {
+            } else if (startDateValid && validator.isLessThanDate(data.endDate, data.startDate)) {
                 state.endDate = 'The lease end date cannot be before the lease start date';
-            } else if (data.effectiveDate && data.endDate < data.effectiveDate) {
+            } else if (data.effectiveDate && validator.isLessThanDate(data.endDate, data.effectiveDate)) {
                 state.endDate = 'The lease end date cannot be before the effective date';
             }
 
