1cbc39a9105a34e771cb6a25727b500ad5aec3ba
christopher.boucher <christopher.boucher@digital.hmrc.gov.uk>
2015-12-21 16:17:40 +0000

    Added test cases for Freehold cases

--- calc-assets/js/test/specs/controller/unit/allinone/resultControllerSpec.js
+++ calc-assets/js/test/specs/controller/unit/allinone/resultControllerSpec.js
@@ -70,10 +70,157 @@
             expect(mockNavigationService.logView.calls.count()).toEqual(1);
         });
         
-         it('should make 1 call to dataService.updateModel', function () {
+        it('should make 1 call to dataService.updateModel', function () {
             expect(mockDataService.updateModel.calls.count()).toEqual(1);
         });
+    });
 
+    describe('Result Controller with valid data - Freehold and Residential', function () {
+        
+        var controller, 
+            mockScope, 
+            mockDataService, 
+            mockNavigationService,
+            mockModelValidationService,
+            mockCalculationService,
+            calledServiceGetModel = false;
+
+        beforeEach(mocks.module('calc.controllers'));
+        beforeEach(mocks.inject(function ($controller, $rootScope) {
+            
+            mockScope = $rootScope.$new();
+
+            mockDataService = { 
+                getModel : function() { 
+                    return {
+                        holdingType : "Freehold",
+                        propertyType : "Residential"
+                    }; 
+                },
+                updateModel : function() { }
+            };
+
+            mockNavigationService = { 
+                logView : function() {} 
+            };
+
+            mockModelValidationService = {
+                validate : function() {
+                    return { isModelValid : true };
+                }
+            };
+
+            mockCalculationService = {
+                calculateResidentialPremiumSlice: function() {},
+                calculateResidentialPremiumSlab: function() {}
+            };
+
+            spyOn(mockDataService, 'getModel').and.callThrough();
+            spyOn(mockNavigationService, 'logView');
+            spyOn(mockCalculationService, 'calculateResidentialPremiumSlice');
+            spyOn(mockCalculationService, 'calculateResidentialPremiumSlab');
+            spyOn(mockDataService, 'updateModel');
+            
+            controller = $controller('resultController', {
+                $scope : mockScope,
+                $location : {},
+                dataService : mockDataService,
+                navigationService : mockNavigationService,
+                modelValidationService : mockModelValidationService,
+                calculationService : mockCalculationService,
+            });
+        }));
+
+        it('should make 1 call to dataService.getModel', function () {
+            expect(mockDataService.getModel.calls.count()).toEqual(1);
+        });
+
+        it('should make 1 call to navigationService.logView', function () {
+            expect(mockNavigationService.logView.calls.count()).toEqual(1);
+        });
+
+        it('should make 1 call to calculationService.calculateResidentialPremiumSlice', function () {
+            expect(mockCalculationService.calculateResidentialPremiumSlice.calls.count()).toEqual(1);
+        });
+
+        it('should make 1 call to calculationService.calculateResidentialPremiumSlab', function () {
+            expect(mockCalculationService.calculateResidentialPremiumSlab.calls.count()).toEqual(1);
+        });
+        
+        it('should make 1 call to dataService.updateModel', function () {
+            expect(mockDataService.updateModel.calls.count()).toEqual(1);
+        });
+    });
+
+    describe('Result Controller with valid data - Freehold and Non-residential', function () {
+        
+        var controller, 
+            mockScope, 
+            mockDataService, 
+            mockNavigationService,
+            mockModelValidationService,
+            mockCalculationService,
+            calledServiceGetModel = false;
+
+        beforeEach(mocks.module('calc.controllers'));
+        beforeEach(mocks.inject(function ($controller, $rootScope) {
+            
+            mockScope = $rootScope.$new();
+
+            mockDataService = { 
+                getModel : function() { 
+                    return {
+                        holdingType : "Freehold",
+                        propertyType : "Non-residential"
+                    }; 
+                },
+                updateModel : function() { }
+            };
+
+            mockNavigationService = { 
+                logView : function() {} 
+            };
+
+            mockModelValidationService = {
+                validate : function() {
+                    return { isModelValid : true };
+                }
+            };
+
+            mockCalculationService = {
+                calculateNonResidentialPremiumSlab: function() {}
+            };
+
+            spyOn(mockDataService, 'getModel').and.callThrough();
+            spyOn(mockNavigationService, 'logView');
+            spyOn(mockCalculationService, 'calculateNonResidentialPremiumSlab');
+            spyOn(mockDataService, 'updateModel');
+            
+            controller = $controller('resultController', {
+                $scope : mockScope,
+                $location : {},
+                dataService : mockDataService,
+                navigationService : mockNavigationService,
+                modelValidationService : mockModelValidationService,
+                calculationService : mockCalculationService,
+            });
+        }));
+
+        it('should make 1 call to dataService.getModel', function () {
+            expect(mockDataService.getModel.calls.count()).toEqual(1);
+        });
+
+        it('should make 1 call to navigationService.logView', function () {
+            expect(mockNavigationService.logView.calls.count()).toEqual(1);
+        });
+
+        it('should make 1 call to calculationService.calculateNonResidentialPremiumSlab', function () {
+            expect(mockCalculationService.calculateNonResidentialPremiumSlab.calls.count()).toEqual(1);
+        });
+        
+        it('should make 1 call to dataService.updateModel', function () {
+            expect(mockDataService.updateModel.calls.count()).toEqual(1);
+        });
     });
 
     describe('Result Controller with invalid data', function () {
