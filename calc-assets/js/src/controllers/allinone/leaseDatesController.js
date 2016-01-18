1ea840be0faaa132bdea48394a2fb89fb2e2f31d
samuel.raspin <samuel.raspin@digital.hmrc.gov.uk>
2016-01-18 16:29:18 +0000

    editted unit tests and changed where the functionality is called

--- calc-assets/js/src/controllers/allinone/leaseDatesController.js
+++ calc-assets/js/src/controllers/allinone/leaseDatesController.js
@@ -15,9 +15,32 @@
         $scope.updateEndDate = function() {
             $scope.data.endDate = dateHelper.parseUIDate($scope.data.endDateYear, $scope.data.endDateMonth, $scope.data.endDateDay);
         };
+        var rentFields = require("../../utilities/displayLeasedYearRentFields");
         $scope.beforeUpdateModel = function() {
             $scope.data.leaseTerm = dateHelper.calculateTermOfLease($scope.data.effectiveDate, $scope.data.startDate, $scope.data.endDate);
+            var yearsToDisplay = rentFields().getFunctions($scope.data);
+            setDisplayYears(yearsToDisplay);
+
         };
+
+        function setDisplayYears(yearsToDisplay) {
+
+            if(!yearsToDisplay.displayYearOneRent){
+                $scope.data.year1Rent = undefined;
+            }
+            if(!yearsToDisplay.displayYearTwoRent){
+                $scope.data.year2Rent = undefined;
+            }
+            if(!yearsToDisplay.displayYearThreeRent){
+                $scope.data.year3Rent = undefined;
+            }
+            if(!yearsToDisplay.displayYearFourRent){
+                $scope.data.year4Rent = undefined;
+            }
+            if(!yearsToDisplay.displayYearFiveRent){
+                $scope.data.year5Rent = undefined;
+            }
+        }
         
     };
 
