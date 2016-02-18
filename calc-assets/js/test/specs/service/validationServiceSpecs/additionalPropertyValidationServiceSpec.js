db185492d4ba845dc16099c79f9d74dc0f9ba4a2
thomas.stacey <thomas.stacey@digital.hmrc.gov.uk>
2016-02-18 16:55:40 +0000

    added validation service spec

--- calc-assets/js/test/specs/service/validationServiceSpecs/additionalPropertyValidationServiceSpec.js
+++ calc-assets/js/test/specs/service/validationServiceSpecs/additionalPropertyValidationServiceSpec.js
@@ -11,33 +11,119 @@
             service = _additionalPropertyValidationService_;
         }));
 
-        it('propertyType.mandatory should return an error when no data provided', function() {
+        it('twoOrMoreProperties.mandatory should return an error when no data provided', function() {
             var state = service.validate({});
             expect(state.isValid).toEqual(false);
-            expect(state.hasError('propertyType')).toEqual('form-field--error');
-            expect(state.validationMessage('propertyType')).toEqual("Provide an answer to continue. Select 'Residential' or 'Non-residential'");
+            expect(state.hasError('twoOrMoreProperties')).toEqual('form-field--error');
+            expect(state.validationMessage('twoOrMoreProperties')).toEqual("Provide an answer to continue. Select 'Yes' or 'No'");
         });
 
-        it('propertyType.format should return no error when "Freehold" selected', function() {
+        it('twoOrMoreProperties.mandatory should return no error when "two or more properties - Yes" selected', function() {
+            var state = service.validate({
+                twoOrMoreProperties : "Yes"
+            });
+            expect(state.isValid).toEqual(false);
+            expect(state.hasError('twoOrMoreProperties')).toEqual('');
+            expect(state.validationMessage('twoOrMoreProperties')).toEqual("");
+        });
+
+        it('twoOrMoreProperties.mandatory should return no error when "two or more properties - No" selected', function() {
+            var state = service.validate({
+                twoOrMoreProperties : "No"
+            });
+            expect(state.isValid).toEqual(true);
+            expect(state.hasError('twoOrMoreProperties')).toEqual('');
+            expect(state.validationMessage('twoOrMoreProperties')).toEqual("");
+        });
+
+
+        it('replaceMainResidence.mandatory should return an error when "two or more properties - Yes" selected and no "replace main residence" selected', function() {
+            var state = service.validate({
+                twoOrMoreProperties : "Yes"
+            });
+            expect(state.isValid).toEqual(false);
+            expect(state.hasError('replaceMainResidence')).toEqual('form-field--error');
+            expect(state.validationMessage('replaceMainResidence')).toEqual("Provide an answer to continue. Select 'Yes' or 'No'");
+        });
+
+
+        it('replaceMainResidence.mandatory should return no error when "two or more properties - No" selected and no "replace main residence" selected', function() {
+            var state = service.validate({
+                twoOrMoreProperties : "No"
+            });
+            expect(state.isValid).toEqual(true);
+            expect(state.hasError('twoOrMoreProperties')).toEqual('');
+            expect(state.validationMessage('twoOrMoreProperties')).toEqual("");
+        });
+
+
+        it('replaceMainResidence.mandatory should return no error when "two or more properties - Yes" selected and no "replace main residence - Yes" selected', function() {
+            var state = service.validate({
+                twoOrMoreProperties : "No",
+                replaceMainResidence : "Yes"
+            });
+            expect(state.isValid).toEqual(true);
+            expect(state.hasError('twoOrMoreProperties')).toEqual('');
+            expect(state.validationMessage('twoOrMoreProperties')).toEqual("");
+        });
+
+
+        it('replaceMainResidence.mandatory should return no error when "two or more properties - Yes" selected and no "replace main residence - No" selected', function() {
+            var state = service.validate({
+                twoOrMoreProperties : "No",
+                replaceMainResidence : "No"
+            });
+            expect(state.isValid).toEqual(true);
+            expect(state.hasError('twoOrMoreProperties')).toEqual('');
+            expect(state.validationMessage('twoOrMoreProperties')).toEqual("");
+        });
+
+
+        it('twoOrMoreProperties.format should return no error when "Yes" selected', function() {
             var form = {
-                propertyType : "residential"
+                twoOrMoreProperties : "Yes"
+            };
+
+            var state = service.validate(form);
+            expect(state.isValid).toEqual(false);
+            expect(state.hasError('twoOrMoreProperties')).toEqual('');
+            expect(state.validationMessage('twoOrMoreProperties')).toEqual("");
+        });
+
+        it('twoOrMoreProperties.format should return no error when "No" selected', function() {
+            var form = {
+                twoOrMoreProperties : "No"
             };
 
             var state = service.validate(form);
             expect(state.isValid).toEqual(true);
-            expect(state.hasError('propertyType')).toEqual('');
-            expect(state.validationMessage('propertyType')).toEqual("");
+            expect(state.hasError('twoOrMoreProperties')).toEqual('');
+            expect(state.validationMessage('twoOrMoreProperties')).toEqual("");
         });
 
-        it('propertyType.format should return no error when "Leasehold" selected', function() {
+
+        it('replaceMainResidence.format should return no error when "Yes" selected', function() {
             var form = {
-                propertyType : "non-residential"
+                twoOrMoreProperties : "Yes",
+                replaceMainResidence : "Yes"
             };
 
             var state = service.validate(form);
             expect(state.isValid).toEqual(true);
-            expect(state.hasError('propertyType')).toEqual('');
-            expect(state.validationMessage('propertyType')).toEqual("");
+            expect(state.hasError('replaceMainResidence')).toEqual('');
+            expect(state.validationMessage('replaceMainResidence')).toEqual("");
+        });
+
+        it('replaceMainResidence.format should return no error when "No" selected', function() {
+            var form = {
+                twoOrMoreProperties : "Yes",
+                replaceMainResidence : "No"
+            };
+
+            var state = service.validate(form);
+            expect(state.isValid).toEqual(true);
+            expect(state.hasError('replaceMainResidence')).toEqual('');
+            expect(state.validationMessage('replaceMainResidence')).toEqual("");
         });
     });
 }());
