55b168a0d35a8ca9e20523abd83b36e858791109
Ufuoma <daniel.ufuoma@digital.hmrc.gov.uk>
2017-10-19 15:19:18 +0100

    Empty tests and  helper slice conversion method added

--- app/calculation/CalculationLogic.scala
+++ app/calculation/CalculationLogic.scala
@@ -1,6 +1,7 @@
 package calculation
 
-import calculation.models.calculationtables.{Slab, SlabResult}
+import calculation.models.SliceDetails
+import calculation.models.calculationtables.{Slab, SlabResult, Slice}
 import play.api.Logger
 
 
@@ -16,6 +17,31 @@ object CalculationLogic {
   def calcTax(amount: BigDecimal, rate: BigDecimal): BigDecimal = {
     Math.floor(Math.floor(amount.toDouble) * rate.toDouble / 100)
   }
+
+  //  def calculateTaxDueSlice(amount: BigDecimal, slices: Seq[Slice]): SliceResult ={
+  //
+  //
+  //  }
+
+  def convertSliceToSliceDetails(amount: BigDecimal, slice: Slice): SliceDetails = {
+    if(slice.to.isDefined) {
+      if (amount > slice.to.get) {
+        SliceDetails(from = slice.from.toInt, to = Some(slice.to.get.toInt), rate = slice.rate.toInt, taxDue = calcTax(slice.to.get - slice.from, slice.rate).toInt)
+      }
+      else if (amount <= slice.from) {
+        SliceDetails(from = slice.from.toInt, to = Some(slice.to.get.toInt), rate = slice.rate.toInt, taxDue = 0)
+      }
+      else {
+        SliceDetails(from = slice.from.toInt, to = Some(slice.to.get.toInt), rate = slice.rate.toInt, taxDue = calcTax(amount - slice.from, slice.rate).toInt)
+      }
+    }else if (amount > slice.from){
+      SliceDetails(from = slice.from.toInt, to = None, rate = slice.rate.toInt, taxDue = calcTax(amount - slice.from, slice.rate).toInt)
+    }
+    else {
+      SliceDetails(from = slice.from.toInt, to = None, rate = slice.rate.toInt, taxDue = 0)
+    }
+  }
+
 
 
 }
