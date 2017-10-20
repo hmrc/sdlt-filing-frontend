77383f0d1c2521b331614426c6de628fa30d60f3
Ufuoma <daniel.ufuoma@digital.hmrc.gov.uk>
2017-10-20 15:47:44 +0100

    Refactored convertSliceToSliceDetails method.

--- app/calculation/CalculationLogic.scala
+++ app/calculation/CalculationLogic.scala
@@ -14,33 +14,36 @@ object CalculationLogic {
       SlabResult(rate = 0, taxDue = 0)}
   }
 
+  def calculateTaxDueSlice(amount: BigDecimal, slices: Seq[Slice]): SliceResult = {
+    val sliceDetails = slices.map(slice => convertSliceToSliceDetails(amount, slice))
+    val totalTaxDue = sliceDetails.foldLeft(0)(_ + _.taxDue)
+    SliceResult(taxDue = totalTaxDue, slices = sliceDetails)
+  }
+
+  private def convertSliceToSliceDetails(amount: BigDecimal, slice: Slice): SliceDetails = {
+    val taxDue = if(amount > slice.from) {
+      calcTaxWithMax(amount - slice.from, slice.rate, slice.to.map(_ - slice.from)).toInt
+    } else 0
+
+    SliceDetails(
+      from = slice.from.toInt,
+      to = slice.to.map(_.toInt),
+      rate = slice.rate.toInt,
+      taxDue = taxDue
+    )
+  }
+
   def calcTax(amount: BigDecimal, rate: BigDecimal): BigDecimal = {
     Math.floor(Math.floor(amount.toDouble) * rate.toDouble / 100)
   }
 
-    def calculateTaxDueSlice(amount: BigDecimal, slices: Seq[Slice]): SliceResult = {
-      val sliceDetails = slices.map(slice => convertSliceToSliceDetails(amount, slice))
-      val totalTaxDue = sliceDetails.foldLeft(0)(_ + _.taxDue)
-      SliceResult(taxDue = totalTaxDue, slices = sliceDetails)
-    }
+  def calcTaxWithMax(amount: BigDecimal, rate: BigDecimal, max: Option[BigDecimal]): BigDecimal = {
+    calcTax(optionalMax(amount, max), rate)
+  }
 
-
-  def convertSliceToSliceDetails(amount: BigDecimal, slice: Slice): SliceDetails = {
-    if(slice.to.isDefined) {
-      if (amount > slice.to.get) {
-        SliceDetails(from = slice.from.toInt, to = Some(slice.to.get.toInt), rate = slice.rate.toInt, taxDue = calcTax(slice.to.get - slice.from, slice.rate).toInt)
-      }
-      else if (amount <= slice.from) {
-        SliceDetails(from = slice.from.toInt, to = Some(slice.to.get.toInt), rate = slice.rate.toInt, taxDue = 0)
-      }
-      else {
-        SliceDetails(from = slice.from.toInt, to = Some(slice.to.get.toInt), rate = slice.rate.toInt, taxDue = calcTax(amount - slice.from, slice.rate).toInt)
-      }
-    }else if (amount > slice.from){
-      SliceDetails(from = slice.from.toInt, to = None, rate = slice.rate.toInt, taxDue = calcTax(amount - slice.from, slice.rate).toInt)
-    }
-    else {
-      SliceDetails(from = slice.from.toInt, to = None, rate = slice.rate.toInt, taxDue = 0)
-    }
+  def optionalMax(amount: BigDecimal, oMax: Option[BigDecimal]): BigDecimal = {
+    oMax.map { max =>
+      if(amount > max) max else amount
+    }.getOrElse(amount)
   }
 }
