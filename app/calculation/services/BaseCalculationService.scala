4f8d8cda44aa7e298fad27168bbb5627c21c77cc
Ufuoma <daniel.ufuoma@digital.hmrc.gov.uk>
2017-10-24 12:04:02 +0100

    Model Validation changes, calculateNPV method made with appropriate tests.

--- app/calculation/services/BaseCalculationService.scala
+++ app/calculation/services/BaseCalculationService.scala
@@ -1,6 +1,6 @@
 package calculation.services
 
-import calculation.models.SliceDetails
+import calculation.models.{LeaseDetails, SliceDetails}
 import calculation.models.calculationtables.{Slab, SlabResult, Slice, SliceResult}
 
 
@@ -19,6 +19,47 @@ object BaseCalculationService {
     val totalTaxDue = sliceDetails.foldLeft(0)(_ + _.taxDue)
     SliceResult(taxDue = totalTaxDue, slices = sliceDetails)
   }
+
+  def calculateNPV(leaseDetails: LeaseDetails): Double ={
+    val rentsList = Seq(Some(leaseDetails.year1Rent),
+                             leaseDetails.year2Rent,
+                             leaseDetails.year3Rent,
+                             leaseDetails.year4Rent,
+                             leaseDetails.year5Rent).flatten
+
+    val fullYears = leaseDetails.leaseTerm.years
+    val partialDays = leaseDetails.leaseTerm.days
+    val daysInYear = leaseDetails.leaseTerm.daysInPartialYear
+
+    val (baseDivisor,baseNPV) = rentsList.foldLeft(BigDecimal(1),0.0){(sum,element) =>
+      val divisor = calcDivisorRate(sum._1)
+      (divisor , sum._2 + (Math.floor(element.toDouble * 1000 / divisor.toDouble) / 1000))
+    }
+
+     Math.floor(if((fullYears >= 5) && (partialDays > 0)) {
+      val rentPartialYear = rentsList.max * partialDays / daysInYear
+      val (divisor, totalNPV) = if(fullYears == 5) (baseDivisor,baseNPV) else calcNPVAbove5Years(baseNPV, 6,  fullYears, rentsList.max)
+       totalNPV + Math.floor(rentPartialYear.toDouble / calcDivisorRate(divisor).toDouble)
+    }
+    else if(fullYears > 5) {
+       calcNPVAbove5Years(baseNPV, 6,  fullYears, rentsList.max)._2}
+    else baseNPV)
+  }
+
+  private def calcDivisorRateScaling(incrementAmount: Int): BigDecimal ={
+    val test = List.fill(incrementAmount)(1.035)
+    test.foldLeft(1.0)((x,y) => x * y)
+  }
+
+  private def calcDivisorRate(currentDivisor: BigDecimal): BigDecimal ={
+    currentDivisor * 1.035
+  }
+
+  def calcNPVAbove5Years(totalNPV: Double, currYear: Int, yearMax: Int, highestRent: BigDecimal): (BigDecimal, Double) ={
+    val currNPV = totalNPV + (Math.floor(highestRent.toDouble * 1000 / calcDivisorRateScaling(currYear).toDouble) / 1000)
+    if(currYear >= yearMax) (calcDivisorRateScaling(currYear).toDouble,currNPV)  else calcNPVAbove5Years(currNPV, currYear+1, yearMax,highestRent)
+  }
+
 
   private def convertSliceToSliceDetails(amount: BigDecimal, slice: Slice): SliceDetails = {
     val taxDue = if(amount > slice.from) {
--- test/calculation/services/BaseCalculationServiceSpec.scala
+++ test/calculation/services/BaseCalculationServiceSpec.scala
@@ -1,6 +1,8 @@
 package calculation.services
 
-import calculation.models.SliceDetails
+import java.time.LocalDate
+
+import calculation.models.{LeaseDetails, LeaseTerm, SliceDetails}
 import calculation.models.calculationtables.{Slab, SlabResult, Slice, SliceResult}
 import uk.gov.hmrc.play.test.UnitSpec
 
@@ -63,22 +65,22 @@ class BaseCalculationServiceSpec extends UnitSpec {
 
     "calculateTaxDueSlice" should {
       val sliceArray = Seq(
-        Slice( from= 0,       to = Some(125000),  rate = 0),
-        Slice( from= 125000,  to = Some(250000),  rate = 2),
-        Slice( from= 250000,  to = Some(925000),  rate = 5),
-        Slice( from= 925000,  to = Some(1500000), rate = 10),
-        Slice( from= 1500000, to = None,   rate = 12))
+        Slice(from = 0, to = Some(125000), rate = 0),
+        Slice(from = 125000, to = Some(250000), rate = 2),
+        Slice(from = 250000, to = Some(925000), rate = 5),
+        Slice(from = 925000, to = Some(1500000), rate = 10),
+        Slice(from = 1500000, to = None, rate = 12))
 
       "return a totalTaxDue of 0" when {
         "the purchase amount is -1" in {
           val calculate = BaseCalculationService.calculateTaxDueSlice(amount = -1, slices = sliceArray)
 
           val result = Seq(
-            SliceDetails( from= 0,       to = Some(125000),  rate = 0, taxDue = 0),
-            SliceDetails( from= 125000,  to = Some(250000),  rate = 2, taxDue = 0),
-            SliceDetails( from= 250000,  to = Some(925000),  rate = 5, taxDue = 0),
-            SliceDetails( from= 925000,  to = Some(1500000), rate = 10, taxDue = 0),
-            SliceDetails( from= 1500000, to = None, rate = 12, taxDue = 0))
+            SliceDetails(from = 0, to = Some(125000), rate = 0, taxDue = 0),
+            SliceDetails(from = 125000, to = Some(250000), rate = 2, taxDue = 0),
+            SliceDetails(from = 250000, to = Some(925000), rate = 5, taxDue = 0),
+            SliceDetails(from = 925000, to = Some(1500000), rate = 10, taxDue = 0),
+            SliceDetails(from = 1500000, to = None, rate = 12, taxDue = 0))
 
           val sliceResult = SliceResult(taxDue = 0, slices = result)
 
@@ -90,27 +92,27 @@ class BaseCalculationServiceSpec extends UnitSpec {
           val calculate = BaseCalculationService.calculateTaxDueSlice(amount = 0, slices = sliceArray)
 
           val result = Seq(
-            SliceDetails( from= 0,       to = Some(125000),  rate = 0, taxDue = 0),
-            SliceDetails( from= 125000,  to = Some(250000),  rate = 2, taxDue = 0),
-            SliceDetails( from= 250000,  to = Some(925000),  rate = 5, taxDue = 0),
-            SliceDetails( from= 925000,  to = Some(1500000), rate = 10, taxDue = 0),
-            SliceDetails( from= 1500000, to = None, rate = 12, taxDue = 0))
+            SliceDetails(from = 0, to = Some(125000), rate = 0, taxDue = 0),
+            SliceDetails(from = 125000, to = Some(250000), rate = 2, taxDue = 0),
+            SliceDetails(from = 250000, to = Some(925000), rate = 5, taxDue = 0),
+            SliceDetails(from = 925000, to = Some(1500000), rate = 10, taxDue = 0),
+            SliceDetails(from = 1500000, to = None, rate = 12, taxDue = 0))
 
           val sliceResult = SliceResult(taxDue = 0, slices = result)
 
           calculate.taxDue shouldBe sliceResult.taxDue
           calculate.slices shouldBe sliceResult.slices
         }
-        
+
         "the purchase amount is 125000" in {
           val calculate = BaseCalculationService.calculateTaxDueSlice(amount = 125000, slices = sliceArray)
 
           val result = Seq(
-            SliceDetails( from= 0,       to = Some(125000),  rate = 0, taxDue = 0),
-            SliceDetails( from= 125000,  to = Some(250000),  rate = 2, taxDue = 0),
-            SliceDetails( from= 250000,  to = Some(925000),  rate = 5, taxDue = 0),
-            SliceDetails( from= 925000,  to = Some(1500000), rate = 10, taxDue = 0),
-            SliceDetails( from= 1500000, to = None, rate = 12, taxDue = 0))
+            SliceDetails(from = 0, to = Some(125000), rate = 0, taxDue = 0),
+            SliceDetails(from = 125000, to = Some(250000), rate = 2, taxDue = 0),
+            SliceDetails(from = 250000, to = Some(925000), rate = 5, taxDue = 0),
+            SliceDetails(from = 925000, to = Some(1500000), rate = 10, taxDue = 0),
+            SliceDetails(from = 1500000, to = None, rate = 12, taxDue = 0))
 
           val sliceResult = SliceResult(taxDue = 0, slices = result)
 
@@ -124,11 +126,11 @@ class BaseCalculationServiceSpec extends UnitSpec {
           val calculate = BaseCalculationService.calculateTaxDueSlice(amount = 125050, slices = sliceArray)
 
           val result = Seq(
-            SliceDetails( from= 0,       to = Some(125000),  rate = 0, taxDue = 0),
-            SliceDetails( from= 125000,  to = Some(250000),  rate = 2, taxDue = 1),
-            SliceDetails( from= 250000,  to = Some(925000),  rate = 5, taxDue = 0),
-            SliceDetails( from= 925000,  to = Some(1500000), rate = 10, taxDue = 0),
-            SliceDetails( from= 1500000, to = None, rate = 12, taxDue = 0))
+            SliceDetails(from = 0, to = Some(125000), rate = 0, taxDue = 0),
+            SliceDetails(from = 125000, to = Some(250000), rate = 2, taxDue = 1),
+            SliceDetails(from = 250000, to = Some(925000), rate = 5, taxDue = 0),
+            SliceDetails(from = 925000, to = Some(1500000), rate = 10, taxDue = 0),
+            SliceDetails(from = 1500000, to = None, rate = 12, taxDue = 0))
 
           val sliceResult = SliceResult(taxDue = 1, slices = result)
 
@@ -142,11 +144,11 @@ class BaseCalculationServiceSpec extends UnitSpec {
           val calculate = BaseCalculationService.calculateTaxDueSlice(amount = 250000, slices = sliceArray)
 
           val result = Seq(
-            SliceDetails( from= 0,       to = Some(125000),  rate = 0, taxDue = 0),
-            SliceDetails( from= 125000,  to = Some(250000),  rate = 2, taxDue = 2500),
-            SliceDetails( from= 250000,  to = Some(925000),  rate = 5, taxDue = 0),
-            SliceDetails( from= 925000,  to = Some(1500000), rate = 10, taxDue = 0),
-            SliceDetails( from= 1500000, to = None, rate = 12, taxDue = 0))
+            SliceDetails(from = 0, to = Some(125000), rate = 0, taxDue = 0),
+            SliceDetails(from = 125000, to = Some(250000), rate = 2, taxDue = 2500),
+            SliceDetails(from = 250000, to = Some(925000), rate = 5, taxDue = 0),
+            SliceDetails(from = 925000, to = Some(1500000), rate = 10, taxDue = 0),
+            SliceDetails(from = 1500000, to = None, rate = 12, taxDue = 0))
 
           val sliceResult = SliceResult(taxDue = 2500, slices = result)
 
@@ -161,11 +163,11 @@ class BaseCalculationServiceSpec extends UnitSpec {
           val calculate = BaseCalculationService.calculateTaxDueSlice(amount = 250020, slices = sliceArray)
 
           val result = Seq(
-            SliceDetails( from= 0,       to = Some(125000),  rate = 0, taxDue = 0),
-            SliceDetails( from= 125000,  to = Some(250000),  rate = 2, taxDue = 2500),
-            SliceDetails( from= 250000,  to = Some(925000),  rate = 5, taxDue = 1),
-            SliceDetails( from= 925000,  to = Some(1500000), rate = 10, taxDue = 0),
-            SliceDetails( from= 1500000, to = None, rate = 12, taxDue = 0))
+            SliceDetails(from = 0, to = Some(125000), rate = 0, taxDue = 0),
+            SliceDetails(from = 125000, to = Some(250000), rate = 2, taxDue = 2500),
+            SliceDetails(from = 250000, to = Some(925000), rate = 5, taxDue = 1),
+            SliceDetails(from = 925000, to = Some(1500000), rate = 10, taxDue = 0),
+            SliceDetails(from = 1500000, to = None, rate = 12, taxDue = 0))
 
           val sliceResult = SliceResult(taxDue = 2501, slices = result)
 
@@ -179,11 +181,11 @@ class BaseCalculationServiceSpec extends UnitSpec {
           val calculate = BaseCalculationService.calculateTaxDueSlice(amount = 1500000, slices = sliceArray)
 
           val result = Seq(
-            SliceDetails( from= 0,       to = Some(125000),  rate = 0, taxDue = 0),
-            SliceDetails( from= 125000,  to = Some(250000),  rate = 2, taxDue = 2500),
-            SliceDetails( from= 250000,  to = Some(925000),  rate = 5, taxDue = 33750),
-            SliceDetails( from= 925000,  to = Some(1500000), rate = 10, taxDue = 57500),
-            SliceDetails( from= 1500000, to = None, rate = 12, taxDue = 0))
+            SliceDetails(from = 0, to = Some(125000), rate = 0, taxDue = 0),
+            SliceDetails(from = 125000, to = Some(250000), rate = 2, taxDue = 2500),
+            SliceDetails(from = 250000, to = Some(925000), rate = 5, taxDue = 33750),
+            SliceDetails(from = 925000, to = Some(1500000), rate = 10, taxDue = 57500),
+            SliceDetails(from = 1500000, to = None, rate = 12, taxDue = 0))
 
           val sliceResult = SliceResult(taxDue = 93750, slices = result)
 
@@ -197,11 +199,11 @@ class BaseCalculationServiceSpec extends UnitSpec {
           val calculate = BaseCalculationService.calculateTaxDueSlice(amount = 1500009, slices = sliceArray)
 
           val result = Seq(
-            SliceDetails( from= 0,       to = Some(125000),  rate = 0, taxDue = 0),
-            SliceDetails( from= 125000,  to = Some(250000),  rate = 2, taxDue = 2500),
-            SliceDetails( from= 250000,  to = Some(925000),  rate = 5, taxDue = 33750),
-            SliceDetails( from= 925000,  to = Some(1500000), rate = 10, taxDue = 57500),
-            SliceDetails( from= 1500000, to = None, rate = 12, taxDue = 1))
+            SliceDetails(from = 0, to = Some(125000), rate = 0, taxDue = 0),
+            SliceDetails(from = 125000, to = Some(250000), rate = 2, taxDue = 2500),
+            SliceDetails(from = 250000, to = Some(925000), rate = 5, taxDue = 33750),
+            SliceDetails(from = 925000, to = Some(1500000), rate = 10, taxDue = 57500),
+            SliceDetails(from = 1500000, to = None, rate = 12, taxDue = 1))
 
           val sliceResult = SliceResult(taxDue = 93751, slices = result)
 
@@ -209,6 +211,169 @@ class BaseCalculationServiceSpec extends UnitSpec {
           calculate.slices shouldBe sliceResult.slices
         }
       }
+    }
+  }
+
+  "calculateNPV" should {
+    "return a result of 9661" when {
+      "given 1 year, 0 partial days with rents [10000,None,None,None,None]" in {
+        val leaseT = LeaseTerm(years = 1,days = 0, daysInPartialYear = 0)
+        val leaseDetails = LeaseDetails(startDate = LocalDate.of(1949, 1, 15),
+          endDate = LocalDate.of(2049, 12,  31),
+          leaseTerm = leaseT,
+          year1Rent = 10000,
+          year2Rent = None,
+          year3Rent = None,
+          year4Rent = None,
+          year5Rent = None
+        )
+        BaseCalculationService.calculateNPV(leaseDetails) shouldBe 9661
+      }
+    }
+
+    "return a result of 19930" when {
+      "given 2 years, 0 partial days with rents [10000,11000,None,None,None]" in {
+        val leaseT = LeaseTerm(years = 2,days = 0, daysInPartialYear = 0)
+        val leaseDetails = LeaseDetails(startDate = LocalDate.of(1949, 1, 15),
+          endDate = LocalDate.of(2049, 12,  31),
+          leaseTerm = leaseT,
+          year1Rent = 10000,
+          year2Rent = Some(11000),
+          year3Rent = None,
+          year4Rent = None,
+          year5Rent = None
+        )
+        BaseCalculationService.calculateNPV(leaseDetails) shouldBe 19930
+      }
+    }
+
+    "return a result of 30753" when {
+      "given 3 years, 0 partial days with rents [10000,11000,12000,None,None]" in {
+        val leaseT = LeaseTerm(years = 3,days = 0, daysInPartialYear = 0)
+        val leaseDetails = LeaseDetails(startDate = LocalDate.of(1949, 1, 15),
+          endDate = LocalDate.of(2049, 12,  31),
+          leaseTerm = leaseT,
+          year1Rent = 10000,
+          year2Rent = Some(11000),
+          year3Rent = Some(12000),
+          year4Rent = None,
+          year5Rent = None
+        )
+        BaseCalculationService.calculateNPV(leaseDetails) shouldBe 30753
+      }
+
+      "return a result of 42082" when {
+        "given 4 years, 0 partial days with rents [10000,11000,12000,13000,None]" in {
+          val leaseT = LeaseTerm(years = 4, days = 0, daysInPartialYear = 0)
+          val leaseDetails = LeaseDetails(startDate = LocalDate.of(1949, 1, 15),
+            endDate = LocalDate.of(2049, 12, 31),
+            leaseTerm = leaseT,
+            year1Rent = 10000,
+            year2Rent = Some(11000),
+            year3Rent = Some(12000),
+            year4Rent = Some(13000),
+            year5Rent = None
+          )
+          BaseCalculationService.calculateNPV(leaseDetails) shouldBe 42082
+        }
+      }
+
+      "return a result of 53870" when {
+        "given 5 years, 0 partial days with rents [10000,11000,12000,13000,14000]" in {
+          val leaseT = LeaseTerm(years = 5, days = 0, daysInPartialYear = 0)
+          val leaseDetails = LeaseDetails(startDate = LocalDate.of(1949, 1, 15),
+            endDate = LocalDate.of(2049, 12, 31),
+            leaseTerm = leaseT,
+            year1Rent = 10000,
+            year2Rent = Some(11000),
+            year3Rent = Some(12000),
+            year4Rent = Some(13000),
+            year5Rent = Some(14000)
+          )
+          BaseCalculationService.calculateNPV(leaseDetails) shouldBe 53870
+        }
+      }
+
+      "return a result of 65259" when {
+        "given 6 years, 0 partial days with rents [10000,11000,12000,13000,14000]" in {
+          val leaseT = LeaseTerm(years = 6, days = 0, daysInPartialYear = 0)
+          val leaseDetails = LeaseDetails(startDate = LocalDate.of(1949, 1, 15),
+            endDate = LocalDate.of(2049, 12, 31),
+            leaseTerm = leaseT,
+            year1Rent = 10000,
+            year2Rent = Some(11000),
+            year3Rent = Some(12000),
+            year4Rent = Some(13000),
+            year5Rent = Some(14000)
+          )
+          BaseCalculationService.calculateNPV(leaseDetails) shouldBe 65259
+        }
+      }
+
+      "return a result of 377,835" when {
+        "given 100 years, 0 partial days with rents [10000,11000,12000,13000,14000]" in {
+          val leaseT = LeaseTerm(years = 100, days = 0, daysInPartialYear = 0)
+          val leaseDetails = LeaseDetails(startDate = LocalDate.of(1949, 1, 15),
+            endDate = LocalDate.of(2049, 12, 31),
+            leaseTerm = leaseT,
+            year1Rent = 10000,
+            year2Rent = Some(11000),
+            year3Rent = Some(12000),
+            year4Rent = Some(13000),
+            year5Rent = Some(14000)
+          )
+          BaseCalculationService.calculateNPV(leaseDetails) shouldBe 377835
+        }
+      }
+
+      "return a result of 54834" when {
+        "given 5 years, 31 partial days and 366 daysInPartialYear with rents [10000,11000,12000,13000,14000]" in {
+          val leaseT = LeaseTerm(years = 5, days = 31, daysInPartialYear = 366)
+          val leaseDetails = LeaseDetails(startDate = LocalDate.of(1949, 1, 15),
+            endDate = LocalDate.of(2049, 12, 31),
+            leaseTerm = leaseT,
+            year1Rent = 10000,
+            year2Rent = Some(11000),
+            year3Rent = Some(12000),
+            year4Rent = Some(13000),
+            year5Rent = Some(14000)
+          )
+          BaseCalculationService.calculateNPV(leaseDetails) shouldBe 54834
+        }
+      }
+
+      "return a result of 272680" when {
+        "given 35 years, 181 partial days and 365 daysInPartialYear with rents [10000,11000,12000,13000,14000]" in {
+          val leaseT = LeaseTerm(years = 35, days = 181, daysInPartialYear = 365)
+          val leaseDetails = LeaseDetails(startDate = LocalDate.of(1949, 1, 15),
+            endDate = LocalDate.of(2049, 12, 31),
+            leaseTerm = leaseT,
+            year1Rent = 10000,
+            year2Rent = Some(11000),
+            year3Rent = Some(12000),
+            year4Rent = Some(13000),
+            year5Rent = Some(14000)
+          )
+          BaseCalculationService.calculateNPV(leaseDetails) shouldBe 272680
+        }
+      }
+
+      "return a result of 237461" when {
+        "given 1 year, 1 partial day and 365 daysInPartialYear with rents [125000, 125000,None,None,None]" in {
+          val leaseT = LeaseTerm(years = 1, days = 1, daysInPartialYear = 365)
+          val leaseDetails = LeaseDetails(startDate = LocalDate.of(1949, 1, 15),
+            endDate = LocalDate.of(2049, 12, 31),
+            leaseTerm = leaseT,
+            year1Rent = 125000,
+            year2Rent = Some(125000),
+            year3Rent = None,
+            year4Rent = None,
+            year5Rent = None
+          )
+          BaseCalculationService.calculateNPV(leaseDetails) shouldBe 237461
+        }
+      }
+
     }
   }
 }
