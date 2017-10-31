96b021c2950fede1169628329a6690bf1ad2ea3a
Ufuoma <daniel.ufuoma@digital.hmrc.gov.uk>
2017-10-31 16:24:55 +0000

    Code refactor, removed excessive tests.

--- app/calculation/exceptions/RequiredValueNotDefinedException.scala
+++ app/calculation/exceptions/RequiredValueNotDefinedException.scala
@@ -1,3 +1,5 @@
 package calculation.exceptions
 
 class RequiredValueNotDefinedException(message: String) extends Throwable(message)
+
+class InvalidDateException(message: String) extends Throwable(message)
--- app/calculation/services/CalculationService.scala
+++ app/calculation/services/CalculationService.scala
@@ -4,7 +4,7 @@ import java.time.LocalDate
 import javax.inject.{Inject, Singleton}
 
 import calculation.enums.{HoldingTypes, PropertyTypes}
-import calculation.exceptions.RequiredValueNotDefinedException
+import calculation.exceptions.{InvalidDateException, RequiredValueNotDefinedException}
 import calculation.models.{CalculationResponse, Request}
 import calculation.data.Dates
 
@@ -19,56 +19,58 @@ trait CalculationSrv{
   val leaseCalculationService: LeaseholdCalculationSrv
   val freeCalculationService: FreeholdCalculationSrv
 
-  def selectCalculationFunction(request: Request): CalculationResponse ={
+  def CalculateTax(request: Request): CalculationResponse ={
     request.holdingType match {
       case HoldingTypes.freehold  =>
         request.propertyType match {
-          case PropertyTypes.residential => freeholdResidentialSelector(request)
-          case PropertyTypes.nonResidential => freeholdNonResidentialSelector(request)
+          case PropertyTypes.residential => calculateFreeholdResidentialTax(request)
+          case PropertyTypes.nonResidential => calculateFreeholdNonResidentialTax(request)
         }
 
       case HoldingTypes.leasehold =>
         request.propertyType match {
-          case PropertyTypes.residential => leaseholdResidentialSelector(request)
-          case PropertyTypes.nonResidential => leaseholdNonResidentialSelector(request)
+          case PropertyTypes.residential => calculateLeaseholdResidentialTax(request)
+          case PropertyTypes.nonResidential => calculateLeaseholdNonResidentialTax(request)
         }
     }
   }
 
-  def freeholdResidentialSelector (request: Request): CalculationResponse = {
-    request.effectiveDate match {
-      case date if compareDates(date, Dates.APRIL2016_RESIDENTIAL_DATE) =>  CalculationResponse(freeCalculationService.freeholdResidentialAddPropApr16Onwards(request))
-      case date if compareDates(date, Dates.DECEMBER2014_RESIDENTIAL_DATE) =>  CalculationResponse(Seq(freeCalculationService.freeholdResidentialDec14Onwards(request)))
-      case date if compareDates(date, Dates.MIN_RESIDENTIAL_DATE) => CalculationResponse(Seq(freeCalculationService.freeholdResidentialMar12toDec14(request)))
-      case _ => throw new RequiredValueNotDefinedException(s"Date of ${request.effectiveDate} is invalid or before 22/3/2012")
+  implicit class DateHelper(dt: LocalDate) {
+    def onOrAfter(compDate: LocalDate): Boolean = {
+      dt.isAfter(compDate) || dt.isEqual(compDate)
     }
   }
 
-  def freeholdNonResidentialSelector (request: Request): CalculationResponse = {
-    if(compareDates(request.effectiveDate, Dates.MARCH2016_NON_RESIDENTIAL_DATE)){
+  def calculateFreeholdResidentialTax (request: Request): CalculationResponse = {
+    request.effectiveDate match {
+      case date if date.onOrAfter(Dates.APRIL2016_RESIDENTIAL_DATE) =>  CalculationResponse(freeCalculationService.freeholdResidentialAddPropApr16Onwards(request))
+      case date if date.onOrAfter(Dates.DECEMBER2014_RESIDENTIAL_DATE) =>  CalculationResponse(Seq(freeCalculationService.freeholdResidentialDec14Onwards(request)))
+      case date if date.onOrAfter(Dates.MIN_RESIDENTIAL_DATE) => CalculationResponse(Seq(freeCalculationService.freeholdResidentialMar12toDec14(request)))
+      case _ => throw new InvalidDateException(s"Date of ${request.effectiveDate} is invalid or before 22/3/2012")
+    }
+  }
+
+  def calculateFreeholdNonResidentialTax (request: Request): CalculationResponse = {
+    if(request.effectiveDate.onOrAfter(Dates.MARCH2016_NON_RESIDENTIAL_DATE)){
       CalculationResponse(freeCalculationService.freeholdNonResidentialMar16Onwards(request))
     }else{
       CalculationResponse(Seq(freeCalculationService.freeholdNonResidentialMar12toMar16(request)))
     }
   }
 
-  def leaseholdResidentialSelector (request: Request): CalculationResponse = {
+  def calculateLeaseholdResidentialTax (request: Request): CalculationResponse = {
     request.effectiveDate match {
-      case date if compareDates(date, Dates.APRIL2016_RESIDENTIAL_DATE) =>  CalculationResponse(leaseCalculationService.leaseholdResidentialAddPropApr16Onwards(request))
-      case date if compareDates(date, Dates.DECEMBER2014_RESIDENTIAL_DATE) =>  CalculationResponse(Seq(leaseCalculationService.leaseholdResidentialDec14Onwards(request)))
-      case date if compareDates(date, Dates.MIN_RESIDENTIAL_DATE) => CalculationResponse(Seq(leaseCalculationService.leaseholdResidentialMar12toDec14(request)))
-      case _ => throw new RequiredValueNotDefinedException(s"Date of ${request.effectiveDate} is invalid or before 22/3/2012")
+      case date if date.onOrAfter(Dates.APRIL2016_RESIDENTIAL_DATE) =>  CalculationResponse(leaseCalculationService.leaseholdResidentialAddPropApr16Onwards(request))
+      case date if date.onOrAfter(Dates.DECEMBER2014_RESIDENTIAL_DATE) =>  CalculationResponse(Seq(leaseCalculationService.leaseholdResidentialDec14Onwards(request)))
+      case date if date.onOrAfter(Dates.MIN_RESIDENTIAL_DATE) => CalculationResponse(Seq(leaseCalculationService.leaseholdResidentialMar12toDec14(request)))
+      case _ => throw new InvalidDateException(s"Date of ${request.effectiveDate} is invalid or before 22/3/2012")
     }
   }
-  def leaseholdNonResidentialSelector (request: Request): CalculationResponse = {
-    if(compareDates(request.effectiveDate, Dates.MARCH2016_NON_RESIDENTIAL_DATE)){
+  def calculateLeaseholdNonResidentialTax (request: Request): CalculationResponse = {
+    if(request.effectiveDate.onOrAfter(Dates.MARCH2016_NON_RESIDENTIAL_DATE)){
       CalculationResponse(leaseCalculationService.leaseholdNonResidentialMar16Onwards(request))
     }else{
       CalculationResponse(Seq(leaseCalculationService.leaseholdNonResidentialMar12toMar16(request)))
     }
-  }
-
-  def compareDates(effectiveDate: LocalDate, lawDate: LocalDate): Boolean ={
-    if(effectiveDate.isAfter(lawDate) || effectiveDate.isEqual(lawDate)) true else false
   }
 }
