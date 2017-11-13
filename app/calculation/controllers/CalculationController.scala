921098dc6885c15dedd8912d3c9858a4f8b5f149
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2017-11-13 10:23:57 +0000

    Fix controller logging

--- app/calculation/controllers/CalculationController.scala
+++ app/calculation/controllers/CalculationController.scala
@@ -28,7 +28,7 @@ trait CalculationCtr extends FrontendController{
               val result = Json.toJson(calculationService.CalculateTax(success.value))
                 Ok(result)
             }else{
-              Logger.error(s"[CalculationController] - Json request body fails model validation with errors: ${ModelValidation.listValidationErrors(success.value)} for request: $request from json: $json.")
+              Logger.error(s"[CalculationController] - Json request body fails model validation with errors: ${ModelValidation.listValidationErrors(success.value)} for request: $success from json: $json.")
               BadRequest(Json.toJson("Validation error: "+ModelValidation.listValidationErrors(success.value)))
             }
           case error: JsError =>
