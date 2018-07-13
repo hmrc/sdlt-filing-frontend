6f3b1c2f48f95f542be8b87ac77d01b293765c22
Robert Webb <robert.webb@digital.hmrc.gov.uk>
2018-07-13 15:50:11 +0100

    added audit mocking

--- test/journey/controllers/ExitSurveyControllerSpec.scala
+++ test/journey/controllers/ExitSurveyControllerSpec.scala
@@ -5,24 +5,34 @@ import akka.stream.ActorMaterializer
 import config.FrontendAuditConnector
 import play.api.http.Status._
 import controllers.ExitSurveyController
+import org.mockito.Matchers
 import org.scalatest.mockito.MockitoSugar
+import org.mockito.Mockito._
 import play.api.libs.json.{JsValue, Json}
 import play.api.test.FakeRequest
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.audit.http.connector.AuditResult
+import uk.gov.hmrc.play.audit.model
+import uk.gov.hmrc.play.audit.model.DataEvent
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
 
 class ExitSurveyControllerSpec extends UnitSpec with MockitoSugar with WithFakeApplication{
 
-  val auditConnector = new FrontendAuditConnector
+  val auditConnector = mock[FrontendAuditConnector]
   val testExitSurveyController = new ExitSurveyController(auditConnector)
   val actorSystem = ActorSystem("actorSystem")
   val materializer: ActorMaterializer = ActorMaterializer()(actorSystem)
+
+  implicit val hc = HeaderCarrier()
 
   "onSubmit" should{
     "throw a BadRequest/400" when{
       "the json is empty" in{
         val fakeRequest = FakeRequest()
         val result = testExitSurveyController.onSubmit(fakeRequest)
-
         status(result) shouldBe BAD_REQUEST
         bodyOf(await(result))(materializer) shouldBe "No Json received"
       }
@@ -59,14 +69,19 @@ class ExitSurveyControllerSpec extends UnitSpec with MockitoSugar with WithFakeA
       }
 
       "given valid completed json" in{
-        val incorrectJson: JsValue = Json.parse(
+        val correctJson: JsValue = Json.parse(
           """
              {
              "radioFeedback": "Satisfied",
              "textFeedback": "This service was perfect, wow, I'm Owen Wilson."
              }""")
 
-        val fakeRequest = FakeRequest().withJsonBody(incorrectJson)
+        val fakeRequest = FakeRequest().withJsonBody(correctJson)
+
+
+        when(auditConnector.sendEvent(Matchers.any())(Matchers.any[HeaderCarrier](), Matchers.any[ExecutionContext]()))
+          .thenReturn(Future.successful(AuditResult.Success))
+
         val result = testExitSurveyController.onSubmit(fakeRequest)
         status(result) shouldBe OK
         bodyOf(await(result))(materializer) shouldBe "Completed Survey"
@@ -85,6 +100,26 @@ class ExitSurveyControllerSpec extends UnitSpec with MockitoSugar with WithFakeA
         status(result) shouldBe OK
         bodyOf(await(result))(materializer) shouldBe "Completed Survey"
       }
+
+      "given valid json & audit event fails" in {
+        val correctJson: JsValue = Json.parse(
+          """
+             {
+             "radioFeedback": "Satisfied",
+             "textFeedback": "This service was perfect, wow, I'm Owen Wilson."
+             }""")
+
+        val fakeRequest = FakeRequest().withJsonBody(correctJson)
+
+
+        when(auditConnector.sendEvent(Matchers.any())(Matchers.any[HeaderCarrier](), Matchers.any[ExecutionContext]()))
+          .thenReturn(Future.failed(new RuntimeException()))
+
+        val result = testExitSurveyController.onSubmit(fakeRequest)
+        status(result) shouldBe OK
+        bodyOf(await(result))(materializer) shouldBe "Completed Survey"
+      }
+
     }
   }
 }
