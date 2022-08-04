55986e0a0fc35554588e9dc879ff4cda660a065c
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-08-04 16:39:03 +0100

    DL-7685 play and bootstrap upgrade plus other dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,8 +6,8 @@ object AppDependencies {
   val compile = Seq(
     filters,
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.20.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "1.31.0-play-28"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.25.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.22.0-play-28"
   )
 
   trait TestDependencies {
@@ -18,14 +18,14 @@ object AppDependencies {
   object Test {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
-        "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.13.1"         % scope,
+        "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.13.3"         % scope,
         "org.scalamock"                %% "scalamock-scalatest-support" % "3.6.0"          % scope,
-        "org.mockito"                  %  "mockito-core"                % "4.3.1"          % scope,
+        "org.mockito"                  %  "mockito-core"                % "4.6.1"          % scope,
         "org.scalatestplus.play"       %% "scalatestplus-play"          % "5.1.0"          % scope,
         "org.scalatestplus"            %% "mockito-3-12"                % "3.2.10.0"       % scope,
         "org.scalatestplus"            %% "scalatestplus-scalacheck"    % "3.1.0.0-RC2"    % scope,
-        "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "5.20.0"         % scope,
-        "org.jsoup"                    %  "jsoup"                       % "1.14.3"         % scope
+        "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "5.25.0"         % scope,
+        "org.jsoup"                    %  "jsoup"                       % "1.15.2"         % scope
       )
     }.test
   }
@@ -36,10 +36,10 @@ object AppDependencies {
       override lazy val scope: String = "it"
 
       override lazy val test = Seq(
-        "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.13.1"         % scope,
-        "com.github.tomakehurst"       %  "wiremock-jre8"               % "2.32.0"         % scope,
+        "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.13.3"         % scope,
+        "com.github.tomakehurst"       %  "wiremock-jre8"               % "2.33.2"         % scope,
         "org.scalatestplus"            %% "mockito-3-12"                % "3.2.10.0"       % scope,
-        "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "5.20.0"         % scope,
+        "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "5.25.0"         % scope,
         "org.scalatest"                %% "scalatest"                   % "3.1.4"          % scope,
         "org.scalatestplus.play"       %% "scalatestplus-play"          % "5.1.0"          % scope
       )
