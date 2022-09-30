83df8951bd284e0c5beed61cc88a7e0f24630e72
Ben Ryan <37295698+benryan1790@users.noreply.github.com>
2022-09-30 15:42:03 +0100

    add tests to currentValueControllerSpec

--- calc-assets/js/test/specs/controller/unit/allinone/currentValueControllerSpec.js
+++ calc-assets/js/test/specs/controller/unit/allinone/currentValueControllerSpec.js
@@ -54,6 +54,98 @@
             expect(mockScope.state.hasError()).toEqual('');
         });
 
+        describe('Calling .ftbLimit() on the Current Value Controller when the date is onOrAfter 23/09/2022', function() {
+
+            beforeEach(mocks.inject(function ($controller, $rootScope) {
+
+                mockScope = $rootScope.$new();
+                mockScope.getHelpSetup = function() {return true;};
+
+                mockDataService = {
+                    getModel : function() {},
+                    updateModel : function() {}
+                };
+
+                mockNavigationService = {
+                    next : function() {}
+                };
+
+                mockValidationService = {
+                    validate : function() {
+                        return { isValid : true };
+                    }
+                };
+
+
+                spyOn(mockDataService, 'updateModel');
+                spyOn(mockNavigationService, 'next');
+                spyOn(mockValidationService, 'validate').and.callThrough();
+
+                controller = $controller('currentValueController', {
+                    $scope : mockScope,
+                    $location : {},
+                    dataService : mockDataService,
+                    currentValueValidationService : mockValidationService,
+                    navigationService : mockNavigationService
+                });
+
+                mockScope.data = {
+                    holdingType: "leasehold"
+                };
+
+            }));
+            it('should return 625,000', function () {
+                mockScope.data.effectiveDate = new Date(2022,8,23);
+                expect(mockScope.ftbLimit()).toEqual("625,000");
+            });
+        });
+
+        describe('Calling .ftbLimit() on the Current Value Controller when the date is before 23/09/2022', function() {
+
+            beforeEach(mocks.inject(function ($controller, $rootScope) {
+
+                mockScope = $rootScope.$new();
+                mockScope.getHelpSetup = function() {return true;};
+
+                mockDataService = {
+                    getModel : function() {},
+                    updateModel : function() {}
+                };
+
+                mockNavigationService = {
+                    next : function() {}
+                };
+
+                mockValidationService = {
+                    validate : function() {
+                        return { isValid : true };
+                    }
+                };
+
+
+                spyOn(mockDataService, 'updateModel');
+                spyOn(mockNavigationService, 'next');
+                spyOn(mockValidationService, 'validate').and.callThrough();
+
+                controller = $controller('currentValueController', {
+                    $scope : mockScope,
+                    $location : {},
+                    dataService : mockDataService,
+                    currentValueValidationService : mockValidationService,
+                    navigationService : mockNavigationService
+                });
+
+                mockScope.data = {
+                    holdingType : "leasehold"
+                };
+
+            }));
+            it('should return 500,000', function () {
+                mockScope.data.effectiveDate = new Date(2022,8,22);
+                expect(mockScope.ftbLimit()).toEqual("500,000");
+            });
+        });
+
         describe('Calling .submit() on the Current Value Controller with invalid data', function () {
 
             beforeEach(mocks.inject(function ($controller, $rootScope) {
