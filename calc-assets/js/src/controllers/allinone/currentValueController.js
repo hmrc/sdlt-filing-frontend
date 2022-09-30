c4e11bf9c85b3cfd39435e96fe3e77680a8f06d5
Ben Ryan <37295698+benryan1790@users.noreply.github.com>
2022-09-30 14:19:20 +0100

    add missing semicolons

--- calc-assets/js/src/controllers/allinone/currentValueController.js
+++ calc-assets/js/src/controllers/allinone/currentValueController.js
@@ -11,12 +11,12 @@
         $scope.ftbLimit = function () {
             var value;
             if($scope.data.effectiveDate >= new Date(2022, 8, 23)) {
-                value = "625,000"
+                value = "625,000";
             } else {
-                value = "500,000"
+                value = "500,000";
             }
             return value;
-        }
+        };
 
     };
 
