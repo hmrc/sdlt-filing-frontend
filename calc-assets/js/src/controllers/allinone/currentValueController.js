da336539e4d4a506f5588220c2b87e6571f4a810
Ben Ryan <37295698+benryan1790@users.noreply.github.com>
2022-09-30 13:55:44 +0100

    Make labels on currentValue page dependant on effectiveDate

--- calc-assets/js/src/controllers/allinone/currentValueController.js
+++ calc-assets/js/src/controllers/allinone/currentValueController.js
@@ -8,6 +8,16 @@
         var init = require("../../utilities/initFormController");
         init($scope, $location, $anchorScroll, 'current-value', dataService, currentValueValidationService, navigationService);
 
+        $scope.ftbLimit = function () {
+            var value;
+            if($scope.data.effectiveDate >= new Date(2022, 8, 23)) {
+                value = "625,000"
+            } else {
+                value = "500,000"
+            }
+            return value;
+        }
+
     };
 
     app.controller('currentValueController', ['$scope', '$location', '$anchorScroll', 'dataService', 'currentValueValidationService', 'navigationService', currentValueController ]);
