a81ec45e1ac995628b3442a17d7ddf7fb23283fb
Ben Ryan <37295698+benryan1790@users.noreply.github.com>
2022-10-05 14:18:45 +0100

    test dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,14 +18,13 @@ object AppDependencies {
   object Test {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
-        "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.13.3"      % scope,
+        "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.13.4"      % scope,
         "org.scalamock"                %% "scalamock-scalatest-support" % "3.6.0"       % scope,
-        "org.mockito"                  %  "mockito-core"                % "4.6.1"       % scope,
-        "org.scalatestplus.play"       %% "scalatestplus-play"          % "5.1.0"       % scope,
+        "org.mockito"                  %  "mockito-core"                % "4.8.0"       % scope,
         "org.scalatestplus"            %% "mockito-3-12"                % "3.2.10.0"    % scope,
         "org.scalatestplus"            %% "scalatestplus-scalacheck"    % "3.1.0.0-RC2" % scope,
-        "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "5.25.0"      % scope,
-        "org.jsoup"                    %  "jsoup"                       % "1.15.2"      % scope
+        "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "7.7.0"      % scope,
+        "org.jsoup"                    %  "jsoup"                       % "1.15.3"      % scope
       )
     }.test
   }
@@ -37,11 +36,9 @@ object AppDependencies {
 
       override lazy val test = Seq(
         "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.13.3"   % scope,
-        "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.33.2"   % scope,
+        "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.34.0"   % scope,
         "org.scalatestplus"            %% "mockito-3-12"           % "3.2.10.0" % scope,
-        "uk.gov.hmrc"                  %% "bootstrap-test-play-28" % "5.25.0"   % scope,
-        "org.scalatest"                %% "scalatest"              % "3.2.13"   % scope,
-        "org.scalatestplus.play"       %% "scalatestplus-play"     % "5.1.0"    % scope,
+        "uk.gov.hmrc"                  %% "bootstrap-test-play-28" % "7.7.0"   % scope,
         "com.vladsch.flexmark"         %  "flexmark-all"           % "0.62.2"   % scope // NB Added for scalatest
       )
     }.test
