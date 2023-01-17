054a2b3fa534b9304dd03f084aca61759d606e01
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2023-01-17 11:43:34 +0000

    DL-9494 update play frontend hmrc to latest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     filters,
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.12.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "4.1.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "6.2.0-play-28"
   )
 
   trait TestDependencies {
