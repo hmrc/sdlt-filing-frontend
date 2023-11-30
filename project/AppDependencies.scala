65106e692e92e4909d4b9020fcf880437c722f6a
Stuart Proctor <112641125+StuartJamesProctor@users.noreply.github.com>
2023-11-30 16:35:00 +0000

    DL-10726 Deleted Jackson module as unnecessary

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -21,32 +21,11 @@ object AppDependencies {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
         "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "8.1.0"       % scope,
-        "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.16.0"      % scope,
         "org.mockito"                  %% "mockito-scala"               % "1.17.30"     % scope,
         "org.jsoup"                    %  "jsoup"                       % "1.17.1"      % scope
       )
     }.test
   }
 
-  val jacksonVersion = "2.16.0"
-  val jacksonDatabindVersion = "2.16.0"
-
-  val jacksonOverrides = Seq(
-    "com.fasterxml.jackson.core" % "jackson-core",
-    "com.fasterxml.jackson.core" % "jackson-annotations",
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8",
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310"
-  ).map(_ % jacksonVersion)
-
-  val jacksonDatabindOverrides = Seq(
-    "com.fasterxml.jackson.core" % "jackson-databind" % jacksonDatabindVersion
-  )
-
-  val akkaSerializationJacksonOverrides = Seq(
-    "com.fasterxml.jackson.dataformat" % "jackson-dataformat-cbor",
-    "com.fasterxml.jackson.module" % "jackson-module-parameter-names",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala",
-  ).map(_ % jacksonVersion)
-
-  def apply() = compile ++ jacksonDatabindOverrides ++ jacksonOverrides ++ akkaSerializationJacksonOverrides ++ Test()
+  def apply() = compile ++ Test()
 }
