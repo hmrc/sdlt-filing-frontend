03d48a38b53b567703f0ad769a3bedad8cdb2df9
rameshmandanapu1 <155470931+rameshmandanapu1@users.noreply.github.com>
2024-02-22 11:17:01 +0000

    Unused code clean

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -12,28 +12,4 @@ import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 @Singleton
 class FrontendAppConfig @Inject()(config: ServicesConfig) {
 
-  private def loadConfig(key: String) = config.getString(key)
-
-  val contactFormServiceIdentifier = "SDLTC"
-  private lazy val contactHost = config.getString("contact-frontend.host")
-
-  private lazy val assetsUrl     = config.getString("assets.url")
-  private lazy val assetsVersion = config.getString("assets.version")
-  lazy val assetsPrefix: String  = assetsUrl + assetsVersion
-
-  lazy val reportAProblemPartialUrl = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
-  lazy val reportAProblemNonJSUrl = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
-  lazy val betaFeedbackUrl = s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier"
-  lazy val betaFeedbackUnauthenticatedUrl = s"$contactHost/contact/beta-feedback-unauthenticated?service=$contactFormServiceIdentifier"
-  lazy val feedbackSurveyUrl: String = loadConfig(s"feedback-survey-frontend.url")
-
-  lazy val cookies: String = loadConfig("urls.footer.cookies")
-  lazy val accessibilityStatement: String = loadConfig("urls.footer.accessibility_statement")
-  lazy val privacy: String = loadConfig("urls.footer.privacy_policy")
-  lazy val termsConditions: String = loadConfig("urls.footer.terms_and_conditions")
-  lazy val govukHelp: String = loadConfig("urls.footer.help_page")
-
-  val trackingConsentUrl: String = config.getString("tracking-consent-frontend.url")
-  val gtmContainer: String = config.getString("tracking-consent-frontend.gtm.container")
-
 }
