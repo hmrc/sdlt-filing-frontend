87feebc21f85781e8b07ae6a3bb009d3db759b7d
rameshmandanapu1 <155470931+rameshmandanapu1@users.noreply.github.com>
2024-06-19 11:37:06 +0100

    DL-13834-hmrc-frontend upgarde to 10.1.0

--- app/validators/api/RequestValidators.scala
+++ app/validators/api/RequestValidators.scala
@@ -14,31 +14,26 @@ import scala.util.{Failure, Success, Try}
 
 object RequestValidators {
 
-  def multiFieldDateReads(prefix: String): Reads[LocalDate] = new Reads[LocalDate] {
-    override def reads(json: JsValue): JsResult[LocalDate] = {
+  def multiFieldDateReads(prefix: String): Reads[LocalDate] = (json: JsValue) => {
 
-      json.validate[(Int, Int, Int)](readDateParts(prefix)).flatMap {
-        case (d, m, y) => Try(LocalDate.of(y, m, d)) match {
-          case Success(date) => JsSuccess(date)
-          case Failure(err) => JsError(s"'$prefix' date could not be parsed. Error: ${err.getMessage}")
-        }
+    json.validate[(Int, Int, Int)](readDateParts(prefix)).flatMap {
+      case (d, m, y) => Try(LocalDate.of(y, m, d)) match {
+        case Success(date) => JsSuccess(date)
+        case Failure(err) => JsError(s"'$prefix' date could not be parsed. Error: ${err.getMessage}")
       }
     }
   }
 
   private def readDateParts(prefix: String):Reads[(Int, Int, Int)] = (
     (__ \ s"${prefix}Day").read[Int] and
-    (__ \ s"${prefix}Month").read[Int] and
-    (__ \ s"${prefix}Year").read[Int]
-  )((day: Int, month: Int, year: Int) => (day, month, year))
+      (__ \ s"${prefix}Month").read[Int] and
+      (__ \ s"${prefix}Year").read[Int]
+    )((day: Int, month: Int, year: Int) => (day, month, year))
 
-  val yesNoToBooleanReads: Reads[Boolean] = new Reads[Boolean] {
-    override def reads(json: JsValue): JsResult[Boolean] = json.validate[String] match {
-      case JsSuccess("Yes", JsPath(_)) => JsSuccess(true)
-      case JsSuccess("No", JsPath(_))  => JsSuccess(false)
-      case JsSuccess(str, JsPath(_)) => JsError(s"'$str' could not be converted to Boolean")
-      case err @JsError(_) => err
-    }
+  val yesNoToBooleanReads: Reads[Boolean] = (json: JsValue) => json.validate[String] match {
+    case JsSuccess("Yes", JsPath(_)) => JsSuccess(true)
+    case JsSuccess("No", JsPath(_)) => JsSuccess(false)
+    case JsSuccess(str, JsPath(_)) => JsError(s"'$str' could not be converted to Boolean")
+    case err@JsError(_) => err
   }
 }
-
