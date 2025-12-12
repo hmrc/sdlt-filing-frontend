3aadfc30c288f65a37bd4793eb5789faa6bd19f3
vierlan <28763492+vierlan@users.noreply.github.com>
2025-12-12 09:19:45 +0000

    Updated libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion         = "10.4.0"
-  private val playVersion              = "12.21.0"
+  private val playVersion              = "12.24.0"
 
   val compile: Seq[ModuleID] = Seq(
     filters,
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % playVersion,
-    "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.3.0"
+    "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.4.0"
   )
   val itDependencies: Seq[ModuleID] = Seq()
 
