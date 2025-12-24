844e1deaaee8157a3a131d21ad713645302c8713
nonebula <147166151+nonebula@users.noreply.github.com>
2025-12-24 10:12:36 +0000

    DTR-2236 - rebased onto main

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -116,8 +116,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   def addTaxesRedirectUrl: String =
     s"$addTaxesFrontendHost/business-account/add-tax/other/land/stamp-duty"
 
-  lazy val sdltManagementHost: String =
-    configuration.get[String]("sdlt-management-frontend.host")
+  lazy val sdltManagementHost: String = configuration.get[String](s"sdlt-management-frontend.host")
 
   def sdltManagementRedirectUrl: String =
     s"$sdltManagementHost/stamp-duty-land-tax-management"
