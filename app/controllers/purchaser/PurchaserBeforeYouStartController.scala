e9399c9d3fa2281a8f8022295f0bcb273a54a3f8
s-draycott <64266413+s-draycott@users.noreply.github.com>
2026-04-01 11:17:21 +0100

    DTR-4078-hotfix moved purchaser check to task list

--- app/controllers/purchaser/PurchaserBeforeYouStartController.scala
+++ app/controllers/purchaser/PurchaserBeforeYouStartController.scala
@@ -35,9 +35,6 @@ class PurchaserBeforeYouStartController @Inject()(
 
   def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
-      request.userAnswers.fullReturn
-        .flatMap(_.purchaser)
-        .filter(_.size > 1)
-        .fold(Ok(view()))(_ => Redirect(controllers.purchaser.routes.PurchaserOverviewController.onPageLoad()))
+      Ok(view())
   }
 }
