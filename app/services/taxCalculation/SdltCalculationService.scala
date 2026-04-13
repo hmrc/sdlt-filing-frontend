4d014f0a79168d827cc7d0435a125a1becc5972a
AyubAz <110174399+ayubAz@users.noreply.github.com>
2026-04-13 22:41:43 +0100

    tidy service

--- app/services/taxCalculation/SdltCalculationService.scala
+++ app/services/taxCalculation/SdltCalculationService.scala
@@ -35,12 +35,16 @@ import scala.util.Try
 @Singleton
 class SdltCalculationService @Inject()(connector: SdltCalculationConnector) {
 
-  // TODO: REMOVE EXPLANATORY COMMENTS
   // TODO: DTR-2815: Must Implement Self-Assessed response for Residential before 2012-03-22
   
-  val logger: Logger = LoggerFactory.getLogger(getClass)
+  private val logger: Logger = LoggerFactory.getLogger(getClass)
 
-  val APR2021_RESIDENTIAL_DATE = LocalDate.of(2021, 4, 1)
+  private val APR2021_RESIDENTIAL_DATE = LocalDate.of(2021, 4, 1)
+  private val FIRST_TIME_BUYER_RELIEF  = "32"
+  private val GRANT_OF_LEASE           = "L"
+  private val YES                      = "YES"
+  private val ANNUAL_RENT_THRESHOLD    = 1000
+  
 
   def calculateStampDutyLandTax(userAnswers: UserAnswers)(implicit hc: HeaderCarrier, ec: scala.concurrent.ExecutionContext): Future[Result] =
     buildRequest(userAnswers) match {
@@ -50,79 +54,78 @@ class SdltCalculationService @Inject()(connector: SdltCalculationConnector) {
           throw new IllegalStateException("Calculation response contained no results")
         ))
       case Left(error) =>
-        logger.error(s"[SdltCalculationService][calculateStampDutyLandTax] failed to build request: $error")
-        Future.failed(new IllegalStateException(error))
+        logger.error(s"[SdltCalculationService][calculateStampDutyLandTax] failed to build request: ${error.message}")
+        Future.failed(new IllegalStateException(error.message))
     }
 
-  private def buildRequest(userAnswers: UserAnswers): Either[String, SdltCalculationRequest] =
+  private def buildRequest(userAnswers: UserAnswers): Either[BuildRequestError, SdltCalculationRequest] =
     for {
-      fullReturn    <- userAnswers.fullReturn.toRight("FullReturn not found in UserAnswers")                                  // RETURN
-      land          <- fullReturn.land.flatMap(_.headOption).toRight("Land not found in FullReturn")                          // LAND
-      transaction   <- fullReturn.transaction.toRight("Transaction not found in FullReturn")                                  // TRANSACTION
-      interestCode  <- land.interestCreatedTransferred.toRight("interestCreatedTransferred not found in Land")                // LAND.INTEREST_TRANSFERRED_CREATED - (1.3) Interest transferred or created
-      propertyCode  <- land.propertyType.toRight("propertyType not found in Land")                                            // LAND.PROPERTY_TYPE - (1.1) Type of property
-      effectiveDate <- transaction.effectiveDate.toRight("effectiveDate not found in Transaction")                            // TRANSACTION.EFFECTIVE_DATE - (1.4) Effective date of transaction
-      parsedDate    <- validateDate(effectiveDate).toRight(s"Failed to parse effectiveDate: $effectiveDate")
-      premium       <- transaction.totalConsideration.toRight("totalConsideration not found in Transaction")                  // TRANSACTION.TOTAL_CONSIDERATION - (1.10) Total consideration
-      transDesc     <- transaction.transactionDescription.toRight("transactionDescription not found in Transaction")          // TRANSACTION.TRANSACTION_DESCRIPTION - (1.2) Description of transaction
-      holdingType   <- getHoldingType(transDesc)
-      leaseDetails  <- fullReturn.lease.fold(Right(None))(buildLeaseDetails(_, transaction, parsedDate))                      // LEASE
-      propertyType  <- getPropertyType(propertyCode)
+      fullReturn    <- userAnswers.fullReturn.toRight(MissingFullReturn)
+      land          <- fullReturn.land.flatMap(_.headOption).toRight(MissingAboutTheLand)
+      transaction   <- fullReturn.transaction.toRight(MissingAboutTheTransaction)
+      interestCode  <- land.interestCreatedTransferred.toRight(MissingLandAnswer("interestCreatedTransferred"))
+      propertyCode  <- land.propertyType.toRight(MissingLandAnswer("propertyType"))
+      propertyType  <- getPropertyType(propertyCode).toRight(UnknownPropertyType(propertyCode))
+      effectiveDate <- transaction.effectiveDate.toRight(MissingTransactionAnswer("effectiveDate"))
+      parsedDate    <- validateDate(effectiveDate).toRight(InvalidDate(effectiveDate))
+      premium       <- transaction.totalConsideration.toRight(MissingTransactionAnswer("totalConsideration"))
+      transDesc     <- transaction.transactionDescription.toRight(MissingTransactionAnswer("transactionDescription"))
+      holdingType   <- getHoldingType(transDesc).toRight(UnknownHoldingType(transDesc))
+      leaseDetails  <- fullReturn.lease.fold(Right(None))(buildLeaseDetails(_, transaction, parsedDate))
     } yield SdltCalculationRequest(
       holdingType         = holdingType,
       propertyType        = propertyType,
       effectiveDateDay    = parsedDate.getDayOfMonth,
       effectiveDateMonth  = parsedDate.getMonthValue,
       effectiveDateYear   = parsedDate.getYear,
-      nonUKResident       = fullReturn.residency.flatMap(_.isNonUkResidents.flatMap(handleNonUkResident(parsedDate, propertyType, _))),     // RESIDENCY.IS_NON_UK_RESIDENTS - Non-UK resident purchaser?
+      nonUKResident       = handleNonUkResident(fullReturn.residency, parsedDate, propertyType),
       premium             = premium,
-      highestRent         = BigDecimal(0),                                                                                    // Rent values are bypassed - declaredNpv is used for leasehold calculation
+      highestRent         = BigDecimal(0),
       propertyDetails     = buildPropertyDetails(propertyCode),
       leaseDetails        = leaseDetails,
       relevantRentDetails = fullReturn.lease.map(buildRelevantRentDetails),
-      firstTimeBuyer      = Some(if (transaction.reliefReason.contains("32")) "Yes" else "No"),                               // TRANSACTION.RELIEF_REASON
-      isLinked            = transaction.isLinked.map(_.toUpperCase == "YES"),                                                 // TRANSACTION.IS_LINKED - (1.13) Is this transaction linked to any other?
-      interestTransferred = Some(interestCode),                                                                               // LAND.INTEREST_TRANSFERRED_CREATED - (1.3) Interest transferred or created
+      firstTimeBuyer      = Some(if (transaction.reliefReason.contains(FIRST_TIME_BUYER_RELIEF)) "Yes" else "No"),
+      isLinked            = transaction.isLinked.map(_.toUpperCase == YES),
+      interestTransferred = Some(interestCode),
       taxReliefDetails    = getTaxReliefDetails(transaction),
       isMultipleLand      = fullReturn.land.map(_.size > 1),
-      declaredNpv         = fullReturn.lease.flatMap(_.netPresentValue.flatMap(toBigDecimal))                                 // LEASE.NET_PRESENT_VALUE - (1.23) Net present value upon which tax is calculated
+      declaredNpv         = fullReturn.lease.flatMap(_.netPresentValue.flatMap(toBigDecimal))
     )
 
-  private def getHoldingType(transactionDesc: String): Either[String, HoldingTypes.Value] =
+  private def getHoldingType(transactionDesc: String): Option[HoldingTypes.Value] =
     TransactionType.parse(Some(transactionDesc)).map {
       case GrantOfLease            => HoldingTypes.leasehold
       case ConveyanceTransfer      => HoldingTypes.freehold
       case ConveyanceTransferLease => HoldingTypes.freehold
       case OtherTransaction        => HoldingTypes.freehold
-    }.toRight(s"Unknown transaction type: $transactionDesc")
+    }
 
-  private def getPropertyType(propertyCode: String): Either[String, PropertyTypes.Value] = propertyCode match {
-    case Residential.toString     => Right(PropertyTypes.residential)
-    case Additional.toString      => Right(PropertyTypes.residential)
-    case Mixed.toString           => Right(PropertyTypes.mixed)
-    case NonResidential.toString  => Right(PropertyTypes.nonResidential)
-    case _                        => Left(s"Unknown property type: $propertyCode")
+  private def getPropertyType(propertyCode: String): Option[PropertyTypes.Value] = propertyCode match {
+    case Residential.toString     => Some(PropertyTypes.residential)
+    case Additional.toString      => Some(PropertyTypes.residential)
+    case Mixed.toString           => Some(PropertyTypes.mixed)
+    case NonResidential.toString  => Some(PropertyTypes.nonResidential)
+    case _                        => None
   }
 
   private def getTaxReliefDetails(transaction: Transaction): Option[TaxReliefDetails] =
     for {
-      claimingRelief <- transaction.claimingRelief                     // TRANSACTION.CLAIMING_RELIEF - (1.9) Are you claiming relief?
-      if claimingRelief.toUpperCase == "YES"
-      reliefReason   <- transaction.reliefReason                       // TRANSACTION.RELIEF_REASON - (1.9) If 'yes', please show the reason
+      claimingRelief <- transaction.claimingRelief
+      if claimingRelief.toUpperCase == YES
+      reliefReason   <- transaction.reliefReason
       reliefCode     <- Try(reliefReason.toInt).toOption
     } yield TaxReliefDetails(
       taxReliefCode   = reliefCode,
       isPartialRelief = Some(transaction.reliefAmount.isDefined)       // TRANSACTION.RELIEF_AMOUNT - (1.9) ... amount remaining chargeable (whole pounds only)
     )
 
-  private def handleNonUkResident(effectiveDate: LocalDate, propertyType: PropertyTypes.Value, isNonUkResident: String): Option[String] = {
+  private def handleNonUkResident(residency: Option[Residency], effectiveDate: LocalDate, propertyType: PropertyTypes.Value): Option[String] =
     if (effectiveDate.isBefore(APR2021_RESIDENTIAL_DATE) && propertyType == PropertyTypes.residential) { None }
-    else                                                  { Some(isNonUkResident.capitalize) }
-  }
+    else { residency.flatMap(_.isNonUkResidents).map(_.capitalize) }
   
   private def buildPropertyDetails(propertyCode: String): Option[PropertyDetails] =
     propertyCode match {
-      case LandTypeOfProperty.Additional.toString =>                                        // LAND.PROPERTY_TYPE - (1.1) Type of property
+      case LandTypeOfProperty.Additional.toString =>
         Some(PropertyDetails(
           individual           = "Yes",
           twoOrMoreProperties  = Some("Yes"),
@@ -140,19 +143,19 @@ class SdltCalculationService @Inject()(connector: SdltCalculationConnector) {
         ))
     }
 
-  private def buildLeaseDetails(lease: Lease, transaction: Transaction, effectiveDate: LocalDate): Either[String, Option[LeaseDetails]] =
+  private def buildLeaseDetails(lease: Lease, transaction: Transaction, effectiveDate: LocalDate): Either[BuildRequestError, Option[LeaseDetails]] =
     for {
-      contractStartDate   <- lease.contractStartDate.toRight("contractStartDate not found in Lease")                            // LEASE.CONTRACT_START_DATE - (1.17) Start date as specified in lease
-      contractEndDate     <- lease.contractEndDate.toRight("contractEndDate not found in Lease")                                // LEASE.CONTRACT_END_DATE - (1.18) End date as specified in lease
-      validStartDate      <- validateDate(contractStartDate).toRight(s"Failed to parse contractStartDate: $contractStartDate")
-      validEndDate        <- validateDate(contractEndDate).toRight(s"Failed to parse contractEndDate: $contractEndDate")
+      contractStartDate   <- lease.contractStartDate.toRight(MissingLeaseAnswer("contractStartDate"))
+      contractEndDate     <- lease.contractEndDate.toRight(MissingLeaseAnswer("contractEndDate"))
+      validStartDate      <- validateDate(contractStartDate).toRight(InvalidDate(contractStartDate))
+      validEndDate        <- validateDate(contractEndDate).toRight(InvalidDate(contractEndDate))
       calculationStartDate = if (effectiveDate.isAfter(validStartDate)) effectiveDate else validStartDate
       years                = Period.between(calculationStartDate, validEndDate.plusDays(1)).getYears
       partialStart         = calculationStartDate.plusYears(years)
       days                 = ChronoUnit.DAYS.between(partialStart, validEndDate.plusDays(1)).toInt
-      daysInPartialYear    = if (years < 5 && days > 0) days else 0                                                              // only set for sub-5-year leases with partial days
-      rentYears            = if (years < 5 && daysInPartialYear > 0) Math.min(years + 1, 5) else Math.min(years, 5)              // matches SDLTC yearsRequired logic
-    } yield Option.when(transaction.transactionDescription.contains("L"))(
+      daysInPartialYear    = if (years < 5 && days > 0) days else 0
+      rentYears            = if (years < 5 && daysInPartialYear > 0) Math.min(years + 1, 5) else Math.min(years, 5)
+    } yield Option.when(transaction.transactionDescription.contains(GRANT_OF_LEASE))(
       LeaseDetails(
         startDateDay    = validStartDate.getDayOfMonth,
         startDateMonth  = validStartDate.getMonthValue,
@@ -165,11 +168,11 @@ class SdltCalculationService @Inject()(connector: SdltCalculationConnector) {
           days = days,
           daysInPartialYear = daysInPartialYear
         ),
-        year1Rent = 0,                                 // Rent values are bypassed - declaredNpv is used for leasehold calculation
-        year2Rent = Option.when(rentYears >= 2)(0),    // Rent values are bypassed - declaredNpv is used for leasehold calculation
-        year3Rent = Option.when(rentYears >= 3)(0),    // Rent values are bypassed - declaredNpv is used for leasehold calculation
-        year4Rent = Option.when(rentYears >= 4)(0),    // Rent values are bypassed - declaredNpv is used for leasehold calculation
-        year5Rent = Option.when(rentYears >= 5)(0)     // Rent values are bypassed - declaredNpv is used for leasehold calculation
+        year1Rent = 0,
+        year2Rent = Option.when(rentYears >= 2)(0),
+        year3Rent = Option.when(rentYears >= 3)(0),
+        year4Rent = Option.when(rentYears >= 4)(0),
+        year5Rent = Option.when(rentYears >= 5)(0)
       )
     )
 
@@ -178,7 +181,7 @@ class SdltCalculationService @Inject()(connector: SdltCalculationConnector) {
       contractPre201603        = Some("Yes"),
       contractVariedPost201603 = Some("No"),
       relevantRent             =
-        if (lease.isAnnualRentOver1000.map(_.toUpperCase).contains("YES")) {    // LEASE.IS_ANNUAL_RENT_OVER_1000 - Is the annual rent £1000 or more?
+        if (lease.isAnnualRentOver1000.map(_.toUpperCase).contains(YES)) {
           Some(ANNUAL_RENT_THRESHOLD)
         } else {
           Some(BigDecimal(0))
@@ -191,5 +194,43 @@ class SdltCalculationService @Inject()(connector: SdltCalculationConnector) {
   private def validateDate(date: String): Option[LocalDate] =
     Try(LocalDate.parse(date)).toOption
   
-  private val ANNUAL_RENT_THRESHOLD: BigDecimal = 1000
+  private sealed trait BuildRequestError {
+    def message: String
+  }
+
+  private case object MissingFullReturn extends BuildRequestError {
+    val message = "FullReturn not found in User answers"
+  }
+
+  private case object MissingAboutTheLand extends BuildRequestError {
+    val message = "Could not find the first land in User answers"
+  }
+
+  private case object MissingAboutTheTransaction extends BuildRequestError {
+    val message = "Could not extract 'About the Transaction' journey answers from Full return"
+  }
+
+  private case class MissingLandAnswer(value: String) extends BuildRequestError {
+    val message = s"Could not find user answer from 'About the Land' journey: $value"
+  }
+  
+  private case class MissingTransactionAnswer(value: String) extends BuildRequestError {
+    val message = s"Could not find user answer from 'About the Transaction' journey: $value"
+  }
+
+  private case class InvalidDate(value: String) extends BuildRequestError {
+    val message = s"Invalid date: $value"
+  }
+  
+  private case class UnknownHoldingType(value: String) extends BuildRequestError {
+    val message = s"Unknown Holding Type found: $value"
+  }
+
+  private case class UnknownPropertyType(value: String) extends BuildRequestError {
+    val message = s"Unknown Property Type found: $value"
+  }
+
+  private case class MissingLeaseAnswer(value: String) extends BuildRequestError {
+    val message = s"Could not find user answer from 'About the Lease': $value"
+  }
 }
