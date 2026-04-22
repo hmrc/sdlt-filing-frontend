b0489def7e44429a65759189b2d285eb583541a8
Jayasree Atmakuru <7058921+jayasreemca@users.noreply.github.com>
2026-04-22 13:35:33 +0100

    DL-18898: SDLTC - Migrate to GOV.UK Service Navigation (#281)
    * DL-18898: Adding service Navigation
    
    * DL-18898: Update the import in layout.scala.html

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,7 +19,7 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-30" % bootstrapVersion  % Test,
-    "org.mockito"         %% "mockito-scala"          % "2.1.0"           % Test,
+    "org.mockito"         %% "mockito-scala"          % "2.2.1"           % Test,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
     "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0"         % Test
   )
