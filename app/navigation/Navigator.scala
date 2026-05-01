902d31e3d49b792ea1316c58c273894d30ab527f
indymoorcroft <172614500+indymoorcroft@users.noreply.github.com>
2026-05-01 16:03:29 +0100

    update Navigation tests

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -320,7 +320,7 @@ class Navigator @Inject()() {
 
   private def leaseholdSelfAssessedRoutes(page: Page): UserAnswers => Call = page match {
     case LeaseholdSelfAssessedPremiumPayableTaxPage    =>
-      _ => controllers.taxCalculation.leaseholdSelfAssessed.routes.LeaseholdSdltNotCalculatedNpvDueController.onPageLoad(NormalMode)
+      _ => controllers.taxCalculation.leaseholdSelfAssessed.routes.LeaseholdSelfAssessedTaxDueOnNpvController.onPageLoad(NormalMode)
     case LeaseholdSelfAssessedNpvTaxPage               => _ => routes.IndexController.onPageLoad() // TODO: TO BE UPDATED
     case LeaseholdSelfAssessedTotalAmountDuePage       => _ => routes.IndexController.onPageLoad() // TODO: TO BE UPDATED
     case LeaseholdSelfAssessedPenaltiesAndInterestPage => _ => routes.IndexController.onPageLoad() // TODO: TO BE UPDATED
