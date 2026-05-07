6c411835bdbc0fce6a041fc08aedcc6ec55e9955
dillimateti <238047753+dillimateti501@users.noreply.github.com>
2026-05-07 14:23:14 +0100

    DTR-3489 Transaction address lookup (#301)
    * DTR-3489 Transaction address look up
    
    * addressed all review comments
    
    * addressed all review comments to titles

---
+++ app/viewmodels/checkAnswers/transaction/TransactionAddressSummary.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.checkAnswers.transaction
+
+import models.UserAnswers
+import models.address.Address.toHtml
+import pages.transaction.TransactionAddressPage
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
+import viewmodels.checkAnswers.summary.SummaryRowResult
+import viewmodels.checkAnswers.summary.SummaryRowResult.{Missing, Row}
+import viewmodels.govuk.summarylist.*
+import viewmodels.implicits.*
+
+object TransactionAddressSummary {
+
+  def row(answers: UserAnswers)(implicit messages: Messages): SummaryRowResult = {
+    val changeRoute = controllers.transaction.routes.TransactionAddressController.redirectToAddressLookupTransaction(Some("change"))
+    val label = messages("transaction.address.checkYourAnswersLabel")
+
+    answers.get(TransactionAddressPage).map { answer =>
+
+      Row(
+        SummaryListRowViewModel(
+          key = label,
+          value = ValueViewModel(HtmlContent(toHtml(answer))),
+          actions = Seq(
+            ActionItemViewModel(
+              "site.change",
+              changeRoute.url
+            ).withVisuallyHiddenText(messages("transaction.address.change.hidden"))
+          )
+        )
+      )
+    }.getOrElse {
+      Missing(changeRoute)
+    }
+  }
+}
