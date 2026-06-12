dc418b7e18b4eea314b5156f33928c810a6536c1
waris1hezb <98065846+waris1hezb@users.noreply.github.com>
2026-06-12 12:15:32 +0100

    DTR-5721: Who are you submitting this return for? - ds-2 screen

---
+++ app/models/submission/WhoAreYouSubmittingFor.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.submission
+
+import models.{Enumerable, WithName}
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.Aliases.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.radios.RadioItem
+
+sealed trait WhoAreYouSubmittingFor
+
+object WhoAreYouSubmittingFor extends Enumerable.Implicits {
+
+  case object PurchaserAuthorised extends WithName("purchaserAuthorised") with WhoAreYouSubmittingFor
+  case object PurchaserApproved   extends WithName("purchaserApproved")   with WhoAreYouSubmittingFor
+  case object Myself              extends WithName("myself")              with WhoAreYouSubmittingFor
+
+  val values: Seq[WhoAreYouSubmittingFor] = Seq(
+    PurchaserAuthorised, PurchaserApproved, Myself
+  )
+
+  def options(implicit messages: Messages): Seq[RadioItem] = values.zipWithIndex.map {
+    case (value, index) =>
+      RadioItem(
+        content = Text(messages(s"submission.whoAreYouSubmittingFor.${value.toString}")),
+        value   = Some(value.toString),
+        id      = Some(s"value_$index")
+      )
+  }
+
+  implicit val enumerable: Enumerable[WhoAreYouSubmittingFor] =
+    Enumerable(values.map(v => v.toString -> v): _*)
+}
