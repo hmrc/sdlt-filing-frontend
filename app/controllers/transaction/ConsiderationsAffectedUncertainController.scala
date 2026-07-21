6382729fbc86f82320fe0b5fb4588297d0ce0106
Katherine Boyfield <44240290+kbfield@users.noreply.github.com>
2026-07-21 15:36:07 +0100

    DTR-6914 Skip deferring payment page when 'no' selected on 'Considerations affected by uncertain future events' (#460)

--- app/controllers/transaction/ConsiderationsAffectedUncertainController.scala
+++ app/controllers/transaction/ConsiderationsAffectedUncertainController.scala
@@ -18,17 +18,20 @@ package controllers.transaction
 
 import controllers.actions.*
 import forms.transaction.ConsiderationsAffectedUncertainFormProvider
-import models.Mode
+import models.land.LandTypeOfProperty.{Mixed, NonResidential}
+import models.{Mode, NormalMode}
 import navigation.Navigator
-import pages.transaction.ConsiderationsAffectedUncertainPage
+import pages.transaction.{ConsiderationsAffectedUncertainPage, TransactionDeferringPaymentPage}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
+import services.land.LandService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.transaction.ConsiderationsAffectedUncertainView
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Success
 
 @Singleton
 class ConsiderationsAffectedUncertainController @Inject()(
@@ -41,6 +44,7 @@ class ConsiderationsAffectedUncertainController @Inject()(
                                        statusCheck: CheckSubmissionStatusAction,
                                        formProvider: ConsiderationsAffectedUncertainFormProvider,
                                        val controllerComponents: MessagesControllerComponents,
+                                       landService: LandService,
                                        view: ConsiderationsAffectedUncertainView
                                      )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
@@ -67,9 +71,22 @@ class ConsiderationsAffectedUncertainController @Inject()(
         value =>
           for {
             updatedAnswers <- Future.fromTry(request.userAnswers.set(ConsiderationsAffectedUncertainPage, value))
-            _ <- sessionRepository.set(updatedAnswers)
+            finalAnswers <- Future.fromTry {
+              if !value then updatedAnswers.remove(TransactionDeferringPaymentPage)
+              else Success(updatedAnswers)
+            }
+            _ <- sessionRepository.set(finalAnswers)
           } yield {
+            if (!value && mode == NormalMode) {
+              landService.getMainLand(request.userAnswers).flatMap(_.propertyType) match {
+                case Some(Mixed.toString | NonResidential.toString) =>
+                  Redirect(controllers.transaction.routes.TransactionUseOfLandOrPropertyController.onPageLoad(mode))
+                case _ =>
+                  Redirect(controllers.transaction.routes.SaleOfBusinessController.onPageLoad(mode))
+              }
+            } else {
               Redirect(navigator.nextPage(ConsiderationsAffectedUncertainPage, mode, updatedAnswers))
+            }
           }
       )
   }
