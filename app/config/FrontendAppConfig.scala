690f44d862686c3a7ade32872bdff47f6214df54
kbfield <44240290+kbfield@users.noreply.github.com>
2026-09-09 15:29:38 +0100

    DTR-8273-hotfix update exit survey url

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -78,9 +78,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val loginUrl: String         = configuration.get[String]("urls.login")
   val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
   val signOutUrl: String       = configuration.get[String]("urls.signOut")
-
-  private val exitSurveyBaseUrl: String = configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
-  val exitSurveyUrl: String             = s"$exitSurveyBaseUrl/feedback/sdlt-filing-frontend"
+  val exitSurveyUrl: String    = configuration.get[String]("urls.feedback")
 
   val languageTranslationEnabled: Boolean =
     configuration.get[Boolean]("features.welsh-translation")
